54bd11186c6873272243c62c14ea7c3e3d077942
Jamie Sinn <james.sinn@sinndevelopment.com>
2017-07-20 20:46:11 -0400

    Random alert is random

--- src/main/java/com/sinndevelopment/askesbot/hooks/StreamLabsHandler.java
+++ src/main/java/com/sinndevelopment/askesbot/hooks/StreamLabsHandler.java
@@ -8,16 +8,43 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class StreamLabsHandler
 {
     public String REFRESH_TOKEN = "";
 
+    private int alertCount = 5;
+
     private TokenLogger token;
+
     public StreamLabsHandler(TokenLogger token)
     {
         this.token = token;
-        REFRESH_TOKEN=token.getLatest();
+        REFRESH_TOKEN = token.getLatest();
+    }
+
+    public boolean sendRandom(String user) throws Exception
+    {
+        int randomNum = ThreadLocalRandom.current().nextInt(1, alertCount + 1);
+
+        Thread.sleep(randomNum * 1000);
+
+        switch (randomNum)
+        {
+            case 1:
+                return sendBoo(user);
+            case 2:
+                return sendHAL(user);
+            case 3:
+                return sendTrains(user);
+            case 4:
+                return sendPewPew(user);
+            case 5:
+                return sendSpook(user);
+            default:
+                return sendTrains(user);
+        }
     }
 
     private String getAccessToken() throws Exception
@@ -50,38 +77,39 @@ public class StreamLabsHandler
 
     public boolean sendBoo(String user) throws Exception
     {
-        return sendStreamLabs("*"+user+"* says boo!",
+        return sendStreamLabs("*" + user + "* says boo!",
                 "http://i.imgur.com/eU9dBRP.png",
                 "http://wat.sinnpi.com/dl/nootnoot.ogg");
     }
 
     public boolean sendHAL(String user) throws Exception
     {
-        return sendStreamLabs("*"+user+"* I'm sorry Libby, I'm afraid I can't do that...",
+        return sendStreamLabs("*" + user + "* I'm sorry Libby, I'm afraid I can't do that...",
                 "http://i.imgur.com/DiSyfXB.png",
                 "http://wat.sinnpi.com/dl/dave.ogg");
     }
 
     public boolean sendTrains(String user) throws Exception
     {
-        return sendStreamLabs("*"+user+"* likes trains too!",
+        return sendStreamLabs("*" + user + "* likes trains too!",
                 "http://i.imgur.com/33oHinu.png",
                 "http://wat.sinnpi.com/dl/trains.ogg");
     }
 
     public boolean sendSpook(String user) throws Exception
     {
-        return sendStreamLabs("*"+user+"* is spooked too!",
+        return sendStreamLabs("*" + user + "* is spooked too!",
                 "https://cdn.discordapp.com/attachments/194128662843490304/329463381612429314/sp00k.PNG",
                 "http://wat.sinnpi.com/dl/spoop.ogg");
     }
 
     public boolean sendPewPew(String user) throws Exception
     {
-        return sendStreamLabs("*"+ user + "* (☞ﾟヮﾟ)☞",
+        return sendStreamLabs("*" + user + "* (☞ﾟヮﾟ)☞",
                 "http://i.imgur.com/8Y8mDzr.png",
                 "http://wat.sinnpi.com/dl/pewpew.ogg");
     }
+
     public boolean sendStreamLabs(String message, String image, String sound) throws Exception
     {
         String url = "https://sinndevelopment.com/oauth/streamlabs/alert.php";
@@ -90,7 +118,7 @@ public class StreamLabsHandler
                 "&message=" + message +
                 "&image=" + image +
                 "&sound=" + sound;
-        URL obj = new URL(url+urlParameters);
+        URL obj = new URL(url + urlParameters);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
@@ -110,7 +138,7 @@ public class StreamLabsHandler
         Main.getLogger().info(jsonObject.toString());
         Main.getLogger().info(response.toString());
 
-        Main.getLogger().info("Attempted Contacting Streamlabs... R:"+con.getResponseCode());
+        Main.getLogger().info("Attempted Contacting Streamlabs... R:" + con.getResponseCode());
         ///////
         return con.getResponseCode() == 200;
     }
---
+++ src/main/java/com/sinndevelopment/askesbot/rewards/RandomAlert.java
@@ -0,0 +1,24 @@
+package com.sinndevelopment.askesbot.rewards;
+
+import com.sinndevelopment.askesbot.data.Viewer;
+
+public class RandomAlert extends Reward
+{
+    public RandomAlert()
+    {
+        super(200, "random");
+    }
+    @Override
+    public boolean redeem(Viewer v, int count)
+    {
+        try
+        {
+            return bot.getStreamLabs().sendRandom(v.getUsername());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
